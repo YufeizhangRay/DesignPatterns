d00604c4f686129140509555af97c2c1d70ad128
Yufei Zhang <42585485+YufeizhangRay@users.noreply.github.com>
2018-12-03 15:23:47 -0500

    Update LazyTwo.java

--- DesignPatterns/src/cn/zyf/design/pattern/singleton/LazyTwo.java
+++ DesignPatterns/src/cn/zyf/design/pattern/singleton/LazyTwo.java
@@ -7,7 +7,7 @@ public class LazyTwo {
 	}
 	
 	private static LazyTwo lazyTwo = null;
-	
+	//synchronized修饰静态方法，锁住整个类，每次调用都需要获取锁，效率低下。
 	public static synchronized LazyTwo getInstance() {
 		if(lazyTwo==null) {
 			lazyTwo = new LazyTwo();
